9ae6cd9c97b1e0c28cc633a89648ce7c017ff22e
Ngin <nguyentrungnguyen20202004@gmail.com>
2025-02-11 01:20:43 +0700

    Add pseudo model, define pipeline for detection

---
+++ app/models/detect.py
@@ -0,0 +1,9 @@
+import torch
+
+class DeepfakeDetector:
+    def __init__(self):
+        self.model = None
+
+    def predict(self, image):
+        results = None
+        return results
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
-from flask import render_template
+from flask import render_template, request, json
 from app import app
+from app.models import DeepfakeDetector
+import base64
+import re
+
+detect_model = DeepfakeDetector()
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +18,25 @@ def detect():
 @app.route('/generate')
 def generate():
     return render_template('df-generate/generate.html', title='Generate')
+
+def convertImage(imgData):
+    imgstr = re.search(r'base64,(.*)', imgData).group(1)
+    with open('camera.png', 'wb') as output:
+        output.write(base64.b64decode(imgstr))
+
+import numpy as np
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    image = data['image']
+    params = data['params']
+
+    convertImage(image)
+    print(image[:100])
+    # results = detect_model.predict('camera.png')
+    results = np.random.rand(1).tolist()[0]
+    
+    return json.dumps({'results': results})
+
+    
